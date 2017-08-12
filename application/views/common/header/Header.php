ec598925bf05a0b711407066c57050ebf9995f6b
dipankar <dipankarnath008@gmail.com>
2017-08-12 23:07:01 +0530

    added home page with catagory icons

---
+++ application/views/common/header/Header.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+
+        <title>index</title>
+
+        <!--  favicon -->
+        <link rel="shortcut icon" href="/assets/img/ico/favicon.png">
+        <!--  apple-touch-icon -->
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/img/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/img/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/img/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="/assets/img/ico/apple-touch-icon-57-precomposed.png">
+
+
+
+        <link href='https://fonts.googleapis.com/css?family=Raleway:400,300,500,700,900' rel='stylesheet' type='text/css'>
+        <!-- Material Icons CSS -->
+        <link href="/assets/fonts/iconfont/material-icons.css" rel="stylesheet">
+        <!-- FontAwesome CSS -->
+        <link href="/assets/fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        <!-- materialize -->
+        <link href="/assets/materialize/css/materialize.min.css" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- shortcodes -->
+        <link href="/assets/css/shortcodes/shortcodesae52.css?v=5" rel="stylesheet">
+        <!-- Style CSS -->
+        <link href="/assets/css/styleae52.css?v=5" rel="stylesheet">
+
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body id="top" class="has-header-search">
+
+    <!-- Top bar start-->
+    <div class="top-bar light-blue visible-md visible-lg">
+        <div class="container">
+            <div class="row">
+                <!-- Social Icon -->
+                <div class="col-md-6">
+                    <!-- Social Icon -->
+                    <ul class="list-inline social-top tt-animate btt">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-tumblr"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-6 text-right">
+                    <ul class="topbar-cta no-margin">
+                        <li class="mr-20">
+                            <a><i class="material-icons mr-10">&#xE0B9;</i>info@materialize.com</a>
+                        </li>
+                        <li>
+                            <a><i class="material-icons mr-10">&#xE0CD;</i> +01 123 456 78</a>
+                        </li>
+                    </ul>
+                </div>
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div>
+    <!-- Top bar end-->
+
+
+    <!--header start-->
+    <header id="header" class="tt-nav nav-border-bottom">
+
+        <div class="header-sticky light-header ">
+
+            <div class="container">
+
+                <div class="search-wrapper">
+                    <div class="search-trigger pull-right">
+                        <div class='search-btn'></div>
+                        <i class="material-icons">&#xE8B6;</i>
+                    </div>
+
+                    <!-- Modal Search Form -->
+                    <i class="search-close material-icons">&#xE5CD;</i>
+                    <div class="search-form-wrapper">
+                        <form action="#" class="white-form">
+                            <div class="input-field">
+                                <input type="text" name="search" id="search">
+                                <label for="search" class="">Search Here...</label>
+                            </div>
+                            <button class="btn pink search-button waves-effect waves-light" type="submit"><i class="material-icons">&#xE8B6;</i></button>
+
+                        </form>
+                    </div>
+                </div><!-- /.search-wrapper -->
+
+                <div id="materialize-menu" class="menuzord">
+
+                    <!--logo start-->
+                    <a href="<?php echo base_url()?>home" class="logo-brand">
+                        <img class="retina" src="assets/img/logo.png" alt=""/>
+                    </a>
+                    <!--logo end-->
+
+                    <!--mega menu start-->
+                    <ul class="menuzord-menu pull-right">
+                        <li class="active"><a href="<?php echo base_url()?>">Home</a></li>
+
+                        <li><a href="<?php echo base_url()?>About">About</a></li>
+
+                        <li><a href="<?php echo base_url()?>Sell">Sell</a></li>
+
+                        <li><a href="<?php echo base_url()?>Company">Company Profile</a></li>
+
+                        <li><a href="<?php echo base_url()?>Inquiry">Inquiry</a></li>
+                    </ul>
+                    <!--mega menu end-->
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--header end-->
---
+++ application/views/pages/Home.php
@@ -0,0 +1,110 @@
+<!--page title start-->
+<section class="page-title pattern-bg ptb-30">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2>Search By Body Type</h2>
+                <span>Sub title</span>
+            </div>
+        </div>
+    </div>
+</section>
+<!--page title end-->
+
+<!-- grid gutter-less style start -->
+<section class="padding-bottom-100">
+    <div class="container">
+        <div class="clients-grid no-gutter">
+            <div class="col-md-2 col-sm-6">
+                <div class="border-box brand-hover">
+                    <a href="#">
+                        <img src="assets/img/truck/cargo.svg" alt="clients" style="width: 177px">
+                    </a>
+                </div><!-- /.border-box -->
+            </div><!-- /.col-md-2 -->
+
+            <div class="col-md-2 col-sm-6">
+                <div class="border-box brand-hover">
+                    <a href="#">
+                        <img src="assets/img/truck/trucking-1.svg" alt="clients" style="width: 177px">
+                    </a>
+                </div><!-- /.border-box -->
+            </div><!-- /.col-md-2 -->
+            <div class="col-md-2 col-sm-6">
+                <div class="border-box brand-hover">
+                    <a href="#">
+                        <img src="assets/img/truck/trucking-6.svg" alt="clients" style="width: 177px">
+                    </a>
+                </div><!-- /.border-box -->
+            </div><!-- /.col-md-2 -->
+            <div class="col-md-2 col-sm-6">
+                <div class="border-box brand-hover">
+                    <a href="#">
+                        <img src="assets/img/truck/trucking-7.svg" alt="clients" style="width: 177px">
+                    </a>
+                </div><!-- /.border-box -->
+            </div><!-- /.col-md-2 -->
+            <div class="col-md-2 col-sm-6">
+                <div class="border-box brand-hover">
+                    <a href="#">
+                        <img src="assets/img/truck/trucking-9.svg" alt="clients" style="width: 177px">
+                    </a>
+                </div><!-- /.border-box -->
+            </div><!-- /.col-md-2 -->
+            <div class="col-md-2 col-sm-6">
+                <div class="border-box brand-hover">
+                    <a href="#">
+                        <img src="assets/img/truck/trucking-17.svg" alt="clients" style="width: 177px">
+                    </a>
+                </div><!-- /.border-box -->
+            </div><!-- /.col-md-2 -->
+
+            <div class="col-md-2 col-sm-6">
+                <div class="border-box brand-hover">
+                    <a href="#">
+                        <img src="assets/img/truck/trucking-26.svg" alt="clients" style="width: 177px">
+                    </a>
+                </div><!-- /.border-box -->
+            </div><!-- /.col-md-2 -->
+
+            <div class="col-md-2 col-sm-6">
+                <div class="border-box brand-hover">
+                    <a href="#">
+                        <img src="assets/img/truck/trucking-12.svg" alt="clients" style="width: 177px">
+                    </a>
+                </div><!-- /.border-box -->
+            </div><!-- /.col-md-2 -->
+            <div class="col-md-2 col-sm-6">
+                <div class="border-box brand-hover">
+                    <a href="#">
+                        <img src="assets/img/truck/trucking-10.svg" alt="clients" style="width: 177px">
+                    </a>
+                </div><!-- /.border-box -->
+            </div><!-- /.col-md-2 -->
+            <div class="col-md-2 col-sm-6">
+                <div class="border-box brand-hover">
+                    <a href="#">
+                        <img src="assets/img/truck/trucking-8.svg" alt="clients" style="width: 177px">
+                    </a>
+                </div><!-- /.border-box -->
+            </div><!-- /.col-md-2 -->
+            <div class="col-md-2 col-sm-6">
+                <div class="border-box brand-hover">
+                    <a href="#">
+                        <img src="assets/img/truck/trucking-5.svg" alt="clients" style="width: 177px">
+                    </a>
+                </div><!-- /.border-box -->
+            </div><!-- /.col-md-2 -->
+            <div class="col-md-2 col-sm-6">
+                <div class="border-box brand-hover">
+                    <a href="#">
+                        <img src="assets/img/truck/trucking-3.svg" alt="clients" style="width: 177px">
+                    </a>
+                </div><!-- /.border-box -->
+            </div><!-- /.col-md-2 -->
+
+        </div><!-- /.clients-grid -->
+
+    </div><!-- /.container -->
+</section>
+<!-- grid gutter-less style end -->
