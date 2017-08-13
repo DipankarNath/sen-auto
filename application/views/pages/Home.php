589595f53d9de8f3ad92cc70cd40fa57b968c642
dipankar <dipankarnath008@gmail.com>
2017-08-13 11:42:42 +0530

    updeted home

--- application/views/pages/Home.php
+++ application/views/pages/Home.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center">
-                <h2>Search By Body Type</h2>
+                <h2 class="section-title text-uppercase">Search By Body Type</h2>
                 <span>Sub title</span>
             </div>
         </div>
@@ -12,7 +12,7 @@
 <!--page title end-->
 
 <!-- grid gutter-less style start -->
-<section class="padding-bottom-100">
+<section class="padding-bottom-50">
     <div class="container">
         <div class="clients-grid no-gutter">
             <div class="col-md-2 col-sm-6">
@@ -108,3 +108,323 @@
     </div><!-- /.container -->
 </section>
 <!-- grid gutter-less style end -->
+
+<section class="">
+    <div class="container">
+        <div class="text-center mb-80">
+            <h2 class="section-title text-uppercase">Recommended Vehicles</h2>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-6">
+                <div class="team-wrapper">
+                    <div class="team-img">
+                        <a href="#"><img src="assets/img/recom/1.jpg" class="img-responsive" alt="Image"></a>
+                    </div><!-- /.team-img -->
+
+                    <div class="team-title">
+                        <h3><a href="#">ＫＯＭＡＴＳＵ　Wheel loader</a></h3>
+                        <span>Rops - Cab</span>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <table class="table table-striped">
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row">Car name</th>
+                                        <td>★ Nissan Diesel / Dump / H17 year (vehicle inspection equipped.) *</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Price</th>
+                                        <td>ASK</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Other</th>
+                                        <td>Vehicle inspection H30 June 26 · I · C · T · With ETC · 240 horsepower · internal dimensions 340 × 205 × 60</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div><!-- /.row -->
+                    </div><!-- /.team-title -->
+
+                    <ul class="team-social-links list-inline text-center">
+                        <a class="waves-effect waves-light btn pink">
+                            <i class="material-icons right">&#xE5C8;</i> Know More</a>
+                    </ul>
+
+                </div><!-- /.team-wrapper -->
+            </div><!-- /.col-md-4 -->
+
+            <div class="col-md-4 col-sm-6">
+                <div class="team-wrapper">
+                    <div class="team-img">
+                        <a href="#"><img src="assets/img/recom/2.jpg" class="img-responsive" alt="Image"></a>
+                    </div><!-- /.team-img -->
+
+                    <div class="team-title">
+                        <h3><a href="#"> Mitsubishi / Canter 2t dump</a></h3>
+                        <span>Dump</span>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <table class="table table-striped">
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row">Car name</th>
+                                        <td>★ Nissan Diesel / Dump / H17 year (vehicle inspection equipped.) *</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Price</th>
+                                        <td>ASK</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Other</th>
+                                        <td>Vehicle inspection H30 June 26 · I · C · T · With ETC · 240 horsepower · internal dimensions 340 × 205 × 60</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div><!-- /.row -->
+                    </div><!-- /.team-title -->
+
+                    <ul class="team-social-links list-inline text-center">
+                        <a class="waves-effect waves-light btn pink">
+                            <i class="material-icons right">&#xE5C8;</i> Know More</a>
+                    </ul>
+
+                </div><!-- /.team-wrapper -->
+            </div><!-- /.col-md-4 -->
+
+            <div class="col-md-4 col-sm-6">
+                <div class="team-wrapper">
+                    <div class="team-img">
+                        <a href="#"><img src="assets/img/recom/3.jpg" class="img-responsive" alt="Image"></a>
+                    </div><!-- /.team-img -->
+
+                    <div class="team-title">
+                        <h3><a href="#">ＫＯＭＡＴＳＵ　Wheel loader</a></h3>
+                        <span>Rops - Cab</span>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <table class="table table-striped">
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row">Car name</th>
+                                        <td>★ Nissan Diesel / Dump / H17 year (vehicle inspection equipped.) *</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Price</th>
+                                        <td>ASK</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Other</th>
+                                        <td>Vehicle inspection H30 June 26 · I · C · T · With ETC · 240 horsepower · internal dimensions 340 × 205 × 60</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div><!-- /.row -->
+                    </div><!-- /.team-title -->
+
+                    <ul class="team-social-links list-inline text-center">
+                        <a class="waves-effect waves-light btn pink">
+                            <i class="material-icons right">&#xE5C8;</i> Know More</a>
+                    </ul>
+
+                </div><!-- /.team-wrapper -->
+            </div><!-- /.col-md-4 -->
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-6">
+                <div class="team-wrapper">
+                    <div class="team-img">
+                        <a href="#"><img src="assets/img/recom/4.jpg" class="img-responsive" alt="Image"></a>
+                    </div><!-- /.team-img -->
+
+                    <div class="team-title">
+                        <h3><a href="#">ＫＯＭＡＴＳＵ　Wheel loader</a></h3>
+                        <span>Rops - Cab</span>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <table class="table table-striped">
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row">Car name</th>
+                                        <td>★ Nissan Diesel / Dump / H17 year (vehicle inspection equipped.) *</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Price</th>
+                                        <td>ASK</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Other</th>
+                                        <td>Vehicle inspection H30 June 26 · I · C · T · With ETC · 240 horsepower · internal dimensions 340 × 205 × 60</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div><!-- /.row -->
+                    </div><!-- /.team-title -->
+
+                    <ul class="team-social-links list-inline text-center">
+                        <a class="waves-effect waves-light btn pink">
+                            <i class="material-icons right">&#xE5C8;</i> Know More</a>
+                    </ul>
+
+                </div><!-- /.team-wrapper -->
+            </div><!-- /.col-md-4 -->
+
+            <div class="col-md-4 col-sm-6">
+                <div class="team-wrapper">
+                    <div class="team-img">
+                        <a href="#"><img src="assets/img/recom/5.jpg" class="img-responsive" alt="Image"></a>
+                    </div><!-- /.team-img -->
+
+                    <div class="team-title">
+                        <h3><a href="#">ＫＯＭＡＴＳＵ　Wheel loader</a></h3>
+                        <span>Rops - Cab</span>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <table class="table table-striped">
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row">Car name</th>
+                                        <td>★ Nissan Diesel / Dump / H17 year (vehicle inspection equipped.) *</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Price</th>
+                                        <td>ASK</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Other</th>
+                                        <td>Vehicle inspection H30 June 26 · I · C · T · With ETC · 240 horsepower · internal dimensions 340 × 205 × 60</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div><!-- /.row -->
+                    </div><!-- /.team-title -->
+
+                    <ul class="team-social-links list-inline text-center">
+                        <a class="waves-effect waves-light btn pink">
+                            <i class="material-icons right">&#xE5C8;</i> Know More</a>
+                    </ul>
+
+                </div><!-- /.team-wrapper -->
+            </div><!-- /.col-md-4 -->
+
+            <div class="col-md-4 col-sm-6">
+                <div class="team-wrapper">
+                    <div class="team-img">
+                        <a href="#"><img src="assets/img/recom/6.jpg" class="img-responsive" alt="Image"></a>
+                    </div><!-- /.team-img -->
+
+                    <div class="team-title">
+                        <h3><a href="#">ＫＯＭＡＴＳＵ　Wheel loader</a></h3>
+                        <span>Rops - Cab</span>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <table class="table table-striped">
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row">Car name</th>
+                                        <td>★ Nissan Diesel / Dump / H17 year (vehicle inspection equipped.) *</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Price</th>
+                                        <td>ASK</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Other</th>
+                                        <td>Vehicle inspection H30 June 26 · I · C · T · With ETC · 240 horsepower · internal dimensions 340 × 205 × 60</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div><!-- /.row -->
+                    </div><!-- /.team-title -->
+
+                    <ul class="team-social-links list-inline text-center">
+                        <a class="waves-effect waves-light btn pink">
+                            <i class="material-icons right">&#xE5C8;</i> Know More</a>
+                    </ul>
+
+                </div><!-- /.team-wrapper -->
+            </div><!-- /.col-md-4 -->
+        </div>
+    </div>
+</section>
+
+<section class="banner-5 bg-fixed parallax-bg overlay dark-5 padding-top-50" data-stellar-background-ratio="0.5">
+    <div class="container">
+        <div class="row mb-20">
+            <col-md-6 class="col-md-offset-6 text-center">
+                <h2 class="section-title text-uppercase white-text">What's New</h2>
+            </col-md-6>
+        </div>
+        <div class="row equal-height-row">
+            <div class="col-md-6">
+                <div id="client-testimonial" class="carousel slide carousel-testimonial text-center gray-bg" data-ride="carousel">
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <div class="avatar">
+                                <img src="assets/img/quote-dark.png" alt="Customer Thumb">
+                            </div>
+                            <div class="content">
+                                <p>Quam adipiscing vestibulum feugiat lacus leo a eget leo convallis sagittis nisi varius eros a imperdiet.Dui elementum ut a vestibulum eu fames hendrerit class conubia consequat curae.</p>
+
+                                <div class="testimonial-meta brand-color">
+                                    Jhon Doe,
+                                    <span>User Interface Designer</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="avatar">
+                                <img src="assets/img/quote-dark.png" alt="Customer Thumb">
+                            </div>
+                            <div class="content">
+                                <p>Quam adipiscing vestibulum feugiat lacus leo a eget leo convallis sagittis nisi varius eros a imperdiet.Dui elementum ut a vestibulum eu fames hendrerit class conubia consequat curae.</p>
+
+                                <div class="testimonial-meta brand-color">
+                                    Elita Karim,
+                                    <span>User Interface Designer</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="avatar">
+                                <img src="assets/img/quote-dark.png" alt="Customer Thumb">
+                            </div>
+                            <div class="content">
+                                <p>Quam adipiscing vestibulum feugiat lacus leo a eget leo convallis sagittis nisi varius eros a imperdiet.Dui elementum ut a vestibulum eu fames hendrerit class conubia consequat curae.</p>
+
+                                <div class="testimonial-meta brand-color">
+                                    Tomas Udoy,
+                                    <span>User Interface Designer</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <!-- Controls -->
+                    <a class="left carousel-control" href="#client-testimonial" role="button" data-slide="prev">
+                        <span class="material-icons" aria-hidden="true">&#xE5C4;</span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#client-testimonial" role="button" data-slide="next">
+                        <span class="material-icons" aria-hidden="true">&#xE5C8;</span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-md-5 hero-thumb equal-height-column">
+                <img src="assets/img/mockup/ipad.png" class="img-responsive " alt="Image">
+            </div>
+        </div>
+    </div><!-- /.container -->
+</section>
