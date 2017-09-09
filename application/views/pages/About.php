fe2f90f4125b99a83fab617747283601d6aad279
Saikat Dey <dsaikat378@gmail.com>
2017-09-09 15:22:10 +0530

    about and company are done

--- application/views/pages/About.php
+++ application/views/pages/About.php
@@ -1,5 +1,24 @@
+<!--page title start-->
+<section class="page-title ptb-50">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>About</h2>
+            </div>
+        </div>
+    </div>
+</section>
+<!--page title end-->
+<!-- contact-form-section -->
+<section class="section-padding">
 <div class="container">
-    <div class="row">
-<h1>This is about</h1>
+       <div class="row">
+        <div class="col-md-12">
+            <p>First time customers Since</p>
+            <p>Shen International Co., Ltd. was founded in 2004, it has been trusted and valued by many customers, and we have been able to continue to operate safely from foundation until today. </p>
+            <p>Thanks to you, you can now purchase used trucks and secondhand heavy machinery, and so on from many customers about purchasing and selling. We will open up a new homepage and will continue to disseminate information that you can use for you. Thank you for your constant patronage. </p>
+            <p>For purchase and sale of used trucks and secondhand heavy machinery, etc., we are receiving inquiries from the website, telephone and fax. Please feel free to consult us for your convenience.</p>
+        </div>
     </div>
 </div>
+</section>
---
+++ application/views/pages/Company.php
@@ -0,0 +1,58 @@
+<!--page title start-->
+<section class="page-title ptb-50">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Company Profile</h2>
+            </div>
+        </div>
+    </div>
+</section>
+<!--page title end-->
+<!-- contact-form-section -->
+<section class="section-padding">
+    <div class="container">
+        <div class="row">
+            <div class=".col-md-3 .col-md-offset-3">
+                <div >
+                    <table class="table table-bordered">
+                        <tbody>
+                        <tr>
+                            <th scope="row" class="active">Company Name</th>
+                            <td>Sen International Co.Ltd</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="active">CEO</th>
+                            <td>Sepang Sen Jaixian</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="active">Establishment</th>
+                            <td>March 2004</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="active">Capital</th>
+                            <td>3,000,000 yen</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="active">Street address</th>
+                            <td>Ozan City, Tochigi ken Ohki Ono 308-3</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="active">TEL / FAX</th>
+                            <td>TEL.0285-49-2119 FAX.0285-49-2111</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="active">Bank</th>
+                            <td>Utsunomiya East Branch of Gunma Bank</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="active">Main work content</th>
+                            <td>Second-hand sales such as cars and special vehicles, purchase</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
